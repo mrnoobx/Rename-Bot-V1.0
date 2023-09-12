6855ac8aefeec14d5a3f3e3de9683fca18b46813
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2023-09-12 18:01:37 +0530

    Update config.py
    Signed-off-by: Sahil Desai  <130888169+sahildesai07@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "4410228")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e73c6f2e8842acdeb8bf8c18628bb772")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6140023675:AAEujcvWz-GDZOezpWCr5jKtwYS7ITazUDA") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "ultroidofficial") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
